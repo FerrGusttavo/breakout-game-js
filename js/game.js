c6dd9a0e0460e35dc5b47cdb487075e00de33415
FerrGusttavo <gustavo.augusto.f@hotmail.com>
2022-05-14 10:16:31 -0300

    inicializando framework

---
+++ js/game.js
@@ -0,0 +1,12 @@
+var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+    preload: preload, create: create, update: update
+});
+
+// cuida de pré-carregar os ativos
+function preload() {}
+
+// é executado uma vez quando tudo está carregado e pronto
+function create() {}
+
+// é executado em cada quadro.
+function update() {}
